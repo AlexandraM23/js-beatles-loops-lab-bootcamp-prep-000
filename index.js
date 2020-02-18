ee685e42445ce9fb5ceb453d3cd4104da810f386
comical-technologist-3881 <alexandra.margarint@vodafone.com>
2020-02-18 14:54:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var result = [];
+
+  for(var i = 0; i < musicians.length() - 1; i++) {
+    var sentence = musicians[i] + ' plays ' + instruments[i]
+    result.push(sentence)
+  }
+
+  return result
+}
